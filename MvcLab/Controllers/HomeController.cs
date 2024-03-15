dace336c143fda0c4e78711acdc0fc369e3a8b55
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-03-15 11:25:52 +0100

    commit

--- MvcLab/Controllers/HomeController.cs
+++ MvcLab/Controllers/HomeController.cs
@@ -9,90 +9,93 @@ namespace MvcLab.Controllers
 {
     public class HomeController : Controller
     {
-
+        // Retrieves a cat by ID for editing
         [HttpGet]
-   public IActionResult EditCat(int id)
-{
-    var cats = GetCatsFromJsonFile();
-    var cat = cats.FirstOrDefault(c => c.Id == id);
-    if (cat == null)
-    {
-        return View("Error");
-    }
-    return View(cat);
-}
+        public IActionResult EditCat(int id)
+        {
+            var cats = GetCatsFromJsonFile();
+            var cat = cats.FirstOrDefault(c => c.Id == id);
+            if (cat == null)
+            {
+                return View("Error");
+            }
+            return View(cat);
+        }
 
+        // Updates cat information
+        [HttpPost]
+        public IActionResult EditCat(CatsModel model)
+        {
+            var cats = GetCatsFromJsonFile();
+            var cat = cats.FirstOrDefault(c => c.Id == model.Id);
+            if (cat == null)
+            {
+                return View("Error");
+            }
 
-[HttpPost]
-public IActionResult EditCat(CatsModel model)
-{
-    var cats = GetCatsFromJsonFile();
-    var cat = cats.FirstOrDefault(c => c.Id == model.Id);
-    if (cat == null)
-    {
-        return View("Error");
-    }
+            cat.Name = model.Name;
+            cat.Breed = model.Breed;
+            cat.Color = model.Color;
+            cat.Age = model.Age;
 
-    cat.Name = model.Name;
-    cat.Breed = model.Breed;
-    cat.Color = model.Color;
-    cat.Age = model.Age;
+            SaveCatsToJsonFile(cats);
 
-    SaveCatsToJsonFile(cats);
+            return RedirectToAction("Index");
+        }
 
-    return RedirectToAction("Index");
-}
+        // Retrieves cats data from JSON file
+        private List<CatsModel> GetCatsFromJsonFile()
+        {
+            string jsonStr = System.IO.File.ReadAllText("cats.json");
+            return JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+        }
 
-    private List<CatsModel> GetCatsFromJsonFile()
-{
-    string jsonStr = System.IO.File.ReadAllText("cats.json");
-    return JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
-}
+        // Saves cats data to JSON file
+        private void SaveCatsToJsonFile(List<CatsModel> cats)
+        {
+            string jsonStr = JsonSerializer.Serialize(cats);
+            System.IO.File.WriteAllText("cats.json", jsonStr);
+        }
 
-private void SaveCatsToJsonFile(List<CatsModel> cats)
-{
-    string jsonStr = JsonSerializer.Serialize(cats);
-    System.IO.File.WriteAllText("cats.json", jsonStr);
-}
+        // Deletes a cat by ID
+        public IActionResult DeleteCat(int id)
+        {
+            var cats = GetCatsFromJsonFile();
+            var cat = cats.FirstOrDefault(c => c.Id == id);
+            if (cat != null)
+            {
+                cats.Remove(cat);
+                SaveCatsToJsonFile(cats);
+            }
+            else
+            {
+                // If the cat was not found, return an error view
+                return View("Error");
+            }
 
-public IActionResult DeleteCat(int id)
-{
-    var cats = GetCatsFromJsonFile();
-    var cat = cats.FirstOrDefault(c => c.Id == id);
-    if (cat != null)
-    {
-        cats.Remove(cat);
-        SaveCatsToJsonFile(cats);
-    }
-    else
-    {
-        // If the cat was not found, return an error view
-        return View("Error");
-    }
+            return RedirectToAction("Index");
+        }
 
-    return RedirectToAction("Index");
-}
-
+        // Displays the list of cats
         public IActionResult Index()
         {
-
             string jsonStr = System.IO.File.ReadAllText("cats.json");
-               
-             var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
-
+            var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
             return View(cats);
         }
 
+        // Displays information about the application
         [Route("/omoss")]
         [Route("/about")]
-       public IActionResult About()
-       {
-        string jsonStr = System.IO.File.ReadAllText("cats.json");    
-        var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
-        ViewBag.TotalCats = cats.Count;
-        return View();
+        public IActionResult About()
+        {
+            string jsonStr = System.IO.File.ReadAllText("cats.json");
+            var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+            ViewBag.TotalCats = cats.Count;
+            return View();
         }
 
+        // Displays the cats view
         [Route("/katter")]
         [Route("/cats")]
         public IActionResult Cats()
@@ -100,6 +103,7 @@ public IActionResult DeleteCat(int id)
             return View();
         }
 
+        // Adds a new cat
         [Route("/katter/add")]
         [HttpPost]
         public async Task<IActionResult> AddCat(CatsModel model)
@@ -142,8 +146,6 @@ public IActionResult DeleteCat(int id)
                 // Handle validation error
                 return View("Error");
             }
-
-            
         }
     }
 }
