742468b73b8af4d7f16958fb3caa20e5addcb895
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-03-15 12:31:57 +0100

    issue with ids in json fixed

--- MvcLab/Controllers/HomeController.cs
+++ MvcLab/Controllers/HomeController.cs
@@ -9,93 +9,90 @@ namespace MvcLab.Controllers
 {
     public class HomeController : Controller
     {
-        // Retrieves a cat by ID for editing
+
         [HttpGet]
-        public IActionResult EditCat(int id)
-        {
-            var cats = GetCatsFromJsonFile();
-            var cat = cats.FirstOrDefault(c => c.Id == id);
-            if (cat == null)
-            {
-                return View("Error");
-            }
-            return View(cat);
-        }
+   public IActionResult EditCat(int id)
+{
+    var cats = GetCatsFromJsonFile();
+    var cat = cats.FirstOrDefault(c => c.Id == id);
+    if (cat == null)
+    {
+        return View("Error");
+    }
+    return View(cat);
+}
 
-        // Updates cat information
-        [HttpPost]
-        public IActionResult EditCat(CatsModel model)
-        {
-            var cats = GetCatsFromJsonFile();
-            var cat = cats.FirstOrDefault(c => c.Id == model.Id);
-            if (cat == null)
-            {
-                return View("Error");
-            }
 
-            cat.Name = model.Name;
-            cat.Breed = model.Breed;
-            cat.Color = model.Color;
-            cat.Age = model.Age;
+[HttpPost]
+public IActionResult EditCat(CatsModel model)
+{
+    var cats = GetCatsFromJsonFile();
+    var cat = cats.FirstOrDefault(c => c.Id == model.Id);
+    if (cat == null)
+    {
+        return View("Error");
+    }
 
-            SaveCatsToJsonFile(cats);
+    cat.Name = model.Name;
+    cat.Breed = model.Breed;
+    cat.Color = model.Color;
+    cat.Age = model.Age;
 
-            return RedirectToAction("Index");
-        }
+    SaveCatsToJsonFile(cats);
 
-        // Retrieves cats data from JSON file
-        private List<CatsModel> GetCatsFromJsonFile()
-        {
-            string jsonStr = System.IO.File.ReadAllText("cats.json");
-            return JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
-        }
+    return RedirectToAction("Index");
+}
 
-        // Saves cats data to JSON file
-        private void SaveCatsToJsonFile(List<CatsModel> cats)
-        {
-            string jsonStr = JsonSerializer.Serialize(cats);
-            System.IO.File.WriteAllText("cats.json", jsonStr);
-        }
+    private List<CatsModel> GetCatsFromJsonFile()
+{
+    string jsonStr = System.IO.File.ReadAllText("cats.json");
+    return JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+}
 
-        // Deletes a cat by ID
-        public IActionResult DeleteCat(int id)
-        {
-            var cats = GetCatsFromJsonFile();
-            var cat = cats.FirstOrDefault(c => c.Id == id);
-            if (cat != null)
-            {
-                cats.Remove(cat);
-                SaveCatsToJsonFile(cats);
-            }
-            else
-            {
-                // If the cat was not found, return an error view
-                return View("Error");
-            }
+private void SaveCatsToJsonFile(List<CatsModel> cats)
+{
+    string jsonStr = JsonSerializer.Serialize(cats);
+    System.IO.File.WriteAllText("cats.json", jsonStr);
+}
 
-            return RedirectToAction("Index");
-        }
+public IActionResult DeleteCat(int id)
+{
+    var cats = GetCatsFromJsonFile();
+    var cat = cats.FirstOrDefault(c => c.Id == id);
+    if (cat != null)
+    {
+        cats.Remove(cat);
+        SaveCatsToJsonFile(cats);
+    }
+    else
+    {
+        // If the cat was not found, return an error view
+        return View("Error");
+    }
 
-        // Displays the list of cats
+    return RedirectToAction("Index");
+}
+
         public IActionResult Index()
         {
+
             string jsonStr = System.IO.File.ReadAllText("cats.json");
-            var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+               
+             var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+
             return View(cats);
         }
 
-        // Displays information about the application
         [Route("/omoss")]
         [Route("/about")]
-        public IActionResult About()
-        {
-            string jsonStr = System.IO.File.ReadAllText("cats.json");
-            var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
-            ViewBag.TotalCats = cats.Count;
-            return View();
+       public IActionResult About()
+       {
+        string jsonStr = System.IO.File.ReadAllText("cats.json");    
+        var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+        ViewBag.TotalCats = cats.Count;
+        return View();
         }
 
-        // Displays the cats view
         [Route("/katter")]
         [Route("/cats")]
         public IActionResult Cats()
@@ -103,49 +100,34 @@ namespace MvcLab.Controllers
             return View();
         }
 
-        // Adds a new cat
         [Route("/katter/add")]
+         [Route("/cats/add")]
         [HttpPost]
-        public async Task<IActionResult> AddCat(CatsModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                string jsonStr;
-                try
-                {
-                    jsonStr = await System.IO.File.ReadAllTextAsync("cats.json");
-                }
-                catch (IOException)
-                {
-                    // Handle error
-                    return View("Error");
-                }
 
-                var cats = JsonSerializer.Deserialize<List<CatsModel>>(jsonStr);
+public async Task<IActionResult> AddCat(CatsModel model)
+{
+    if (ModelState.IsValid)
+    {
+        var cats = GetCatsFromJsonFile();
+        
+        // Assign a unique ID to the new cat
+        int maxId = cats.Max(c => c.Id);
+        model.Id = maxId + 1;
+        
+        cats.Add(model);
+        
+        SaveCatsToJsonFile(cats);
 
-                if (cats != null)
-                {
-                    cats.Add(model);
-                    jsonStr = JsonSerializer.Serialize(cats);
-                    try
-                    {
-                        await System.IO.File.WriteAllTextAsync("cats.json", jsonStr);
-                    }
-                    catch (IOException)
-                    {
-                        // Handle error
-                        return View("Error");
-                    }
-                }
-                ModelState.Clear();
+        ModelState.Clear();
 
-                return RedirectToAction("Index", "Home");
-            }
-            else
-            {
-                // Handle validation error
-                return View("Error");
-            }
-        }
+        return RedirectToAction("Index", "Home");
+    }
+    else
+    {
+        // Handle validation error
+        return View("Cats", model);
+    }
+}
+
     }
 }
